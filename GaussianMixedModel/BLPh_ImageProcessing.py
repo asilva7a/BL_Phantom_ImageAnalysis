7394c97ca8c76b1c9dedfe32001d3de0d783c48a
asilva7a <silva7a@cmich.edu>
2024-02-08 12:09:33 -0500

    Added new module for processing the image files prior to segmentation

---
+++ GaussianMixedModel/BLPh_ImageProcessing.py
@@ -0,0 +1,67 @@
+import os
+import pandas as pd
+from PIL import Image
+import numpy as np
+import cv2
+
+class ImageAnalysis:
+    def __init__(self, project_folder):
+        self.project_folder = project_folder
+        self.directory_df = self.initialize_directory_df() 
+        
+    def initialize_directory_df(self):
+        directories = [d for d in os.listdir(self.project_folder) if os.path.isdir(os.path.join(self.project_folder, d))]
+        directory_data = [{'directory_name': d, 'directory_path': os.path.join(self.project_folder, d)} for d in directories]
+        return pd.DataFrame(directory_data, columns=['directory_name', 'directory_path'])
+    
+    def list_directories(self):
+        return [d for d in os.listdir(self.project_folder) if os.path.isdir(os.path.join(self.project_folder, d))]
+    
+    def list_files(self, folder_name):
+        folder_path = os.path.join(self.project_folder, folder_name)
+        all_files = []
+        for root, dirs, files in os.walk(folder_path):
+            for file in files:
+                all_files.append(os.path.join(root, file))
+        return all_files
+    
+    
+    def generate_dark_image(self, tiff_path, num_frames=200):
+        """
+        Generates a median 'dark' image from the first specified number of frames in a multi-frame TIFF file.
+
+        This method is used for compensating the dark pixel offset in bioluminescence imaging data.
+
+        Parameters:
+        tiff_path (str): Path to the multi-frame TIFF file.
+        num_frames (int, optional): Number of frames to consider for generating the dark image. Defaults to 200.
+
+        Returns:
+        numpy.ndarray: A median image representing the 'dark' image.
+        """
+        with Image.open(tiff_path) as img:
+            frames = [np.array(img.getdata(), dtype=np.float32).reshape(img.size[::-1]) for i in range(num_frames)]
+            median_frame = np.median(frames, axis=0)
+            return median_frame
+
+    def subtract_dark_image(self, raw_tiff_path, dark_image):
+        """
+        Subtracts a 'dark' image from each frame of a multi-frame TIFF file.
+
+        This method is used to compensate for the average dark pixel offset in bioluminescence imaging data.
+
+        Parameters:
+        raw_tiff_path (str): Path to the raw multi-frame TIFF file.
+        dark_image (numpy.ndarray): The 'dark' image to be subtracted from each frame of the raw image.
+
+        Returns:
+        list of numpy.ndarray: A list of images, each representing a frame from the raw image with the dark image subtracted.
+        """
+        with Image.open(raw_tiff_path) as img:
+            compensated_images = []
+            for i in range(img.n_frames):
+                img.seek(i)
+                frame = np.array(img.getdata(), dtype=np.float32).reshape(img.size[::-1])
+                compensated_image = cv2.subtract(frame, dark_image)
+                compensated_images.append(compensated_image)
+            return compensated_images
