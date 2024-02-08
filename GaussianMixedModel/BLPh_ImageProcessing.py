0df535442512db2e5970c67f8b65367a9021d6dc
asilva7a <silva7a@cmich.edu>
2024-02-08 12:38:37 -0500

    Added more to the image processing script

--- GaussianMixedModel/BLPh_ImageProcessing.py
+++ GaussianMixedModel/BLPh_ImageProcessing.py
@@ -4,26 +4,61 @@ from PIL import Image
 import numpy as np
 import cv2
 
-class ImageAnalysis:
-    def __init__(self, project_folder):
-        self.project_folder = project_folder
-        self.directory_df = self.initialize_directory_df() 
-        
-    def initialize_directory_df(self):
-        directories = [d for d in os.listdir(self.project_folder) if os.path.isdir(os.path.join(self.project_folder, d))]
-        directory_data = [{'directory_name': d, 'directory_path': os.path.join(self.project_folder, d)} for d in directories]
-        return pd.DataFrame(directory_data, columns=['directory_name', 'directory_path'])
+class directory_manager:
+    """
+    Data Wrangling Class to manage the directories and files in the project folder.
+
+    Loops through the directories in the project folder and lists the files in each directory.
+
+    Parameters:
+    project_folder (str): Path to the project folder.
+
+    returns:
+    list of directories in the project folder
+    """
+    # project_folder is the folder where the data is stored
+    def __init__(self, project_folder): # project_folder is the folder where the data is stored
+        self.project_folder = project_folder # project_folder is the folder where the data is stored
+        self.directory_df = self.initialize_directory_df() # directory_df is a dataframe that contains the names and paths of the directories in the project folder
     
-    def list_directories(self):
-        return [d for d in os.listdir(self.project_folder) if os.path.isdir(os.path.join(self.project_folder, d))]
+    # initialize a dataframe that contains the names and paths of the directories in the project folder
+    def initialize_directory_df(self): # initialize a dataframe that contains the names and paths of the directories in the project folder
+        directories = [d for d in os.listdir(self.project_folder) if os.path.isdir(os.path.join(self.project_folder, d))] # list of directories in the project folder
+        directory_data = [{'directory_name': d, 'directory_path': os.path.join(self.project_folder, d)} for d in directories] # list of dictionaries containing the names and paths of the directories
+        return pd.DataFrame(directory_data, columns=['directory_name', 'directory_path']) # dataframe containing the names and paths of the directories
     
-    def list_files(self, folder_name):
+    # list of directories in the project folder
+    def list_directories(self): # list of directories in the project folder
+        return [d for d in os.listdir(self.project_folder) if os.path.isdir(os.path.join(self.project_folder, d))] # list of directories in the project folder
+    
+    # list of files in the directory
+    def list_files(self, folder_name): # list of directories in the project folder
+        folder_path = os.path.join(self.project_folder, folder_name) # path to the directory    
+        all_files = [] # list of files in the directory
+        for root, dirs, files in os.walk(folder_path): # walk through the directory
+            for file in files: # list of files in the directory
+                all_files.append(os.path.join(root, file)) # list of files in the directory
+        return all_files # list of files in the directory
+    
+    # list of files in those files
+    def list_files_in_files(self, folder_name): # list of files in those files
         folder_path = os.path.join(self.project_folder, folder_name)
         all_files = []
         for root, dirs, files in os.walk(folder_path):
             for file in files:
                 all_files.append(os.path.join(root, file))
         return all_files
+    
+    
+
+    
+
+    
+    def read_tiff(self, tiff_path): # read a multi-frame TIFF file
+        with Image.open(tiff_path) as img:
+            frames = [np.array(img.getdata(), dtype=np.float32).reshape(img.size[::-1]) for i in range(img.n_frames)] # list of frames in the multi-frame TIFF file
+        return frames
+    
     
     
     def generate_dark_image(self, tiff_path, num_frames=100):
@@ -65,3 +100,5 @@ class ImageAnalysis:
                 compensated_image = cv2.subtract(frame, dark_image)
                 compensated_images.append(compensated_image)
             return compensated_images
+        
+    def generate_roi (self, compensated_images,)
